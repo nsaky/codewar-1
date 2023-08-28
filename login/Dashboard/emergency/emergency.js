4e0543dd4778fe25f8866d6e6bd7635da7fecd78
nsaky <140412597+nsaky@users.noreply.github.com>
2023-08-29 00:34:57 +0530

    much work done

---
+++ login/Dashboard/emergency/emergency.js
@@ -0,0 +1,29 @@
+// Danger SOund
+let dangerSound = () => new Audio("Sounds/Sirens/Danger_alert.mp3").play()
+
+//Ambulance 
+let ambulanceSound = () => new Audio("Sounds/Sirens/ambulance.mp3").play()
+
+//police
+let policeSound = () => new Audio("Sounds/Sirens/police.mp3").play()
+
+//doorbell
+let doorBellSound = () => new Audio("Sounds/Distraction/doorbell.mp3").play()
+
+//faucet
+let faucetSound = () => new Audio("Sounds/Distraction/faucet.mp3").play()
+
+//Keyboard Typing
+let keyboardSound = () => new Audio("Sounds/Office/keyboard.mp3").play()
+
+//Breaking Glass
+let glassSound = () => new Audio("Sounds/Distraction/glass.mp3").play()
+
+//Explosion
+let explosionSound = () => new Audio("Sounds/violent/explosion.mp3").play()
+
+//Pistol
+let pistolSound = () => new Audio("Sounds/violent/pistol.mp3").play()
+
+//Rifle
+let rifleSound = () => new Audio("Sounds/violent/rifle.mp3").play()
---
+++ login/login/init.js
@@ -0,0 +1,13 @@
+(function () {
+    var firebaseConfig = {
+
+            apiKey: "AIzaSyBvS82XdP_4YilhMwfSyw6wvsObkcEEp6s",
+            authDomain: "dada-con.firebaseapp.com",
+            projectId: "dada-con",
+            storageBucket: "dada-con.appspot.com",
+            messagingSenderId: "343128068340",
+            appId: "1:343128068340:web:8584d21fbd0bc5bf5a8ea1",
+            measurementId: "G-Q67MT0SGLT"
+    };
+    firebase.initializeApp(firebaseConfig);
+}());
